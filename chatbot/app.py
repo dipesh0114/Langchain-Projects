990408511160487f2cc6c2c5405cdfbf0bdbc3a4
Dipesh <dipeshpatro@gmail.com>
2025-08-04 18:38:51 +0530

    as

---
+++ chatbot/app.py
@@ -0,0 +1,36 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+
+import streamlit as st
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+
+## prompt template
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+
+st.title('langchain demo with openapi')
+input_text = st.text_input("search the topic you want")
+
+llm = ChatOpenAI(model = "gpt-3.5")
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
